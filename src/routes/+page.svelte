a7d4159cf6ab2a752dcb778042fe55ac69bca3d9
JollyGrin <lol@dean.lol>
2025-04-10 15:38:52 +0200

    add shineborder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import Marquee from '$lib/components/Marquee.svelte';
+	import ShineBorder from '$lib/components/ShineBorder.svelte';
 	import { SOL_SYMBOL } from '$lib/symbols';
 
 	const agents = Array.from({ length: 20 }).map((_, i) => ({ name: `Agent${i}` }));
 </script>
 
-<div class="bg-brand-fore/10 border-b-brand-fore/20 border-b-1 px-2 py-4">
-	<div class=" container mx-auto flex justify-between">
-		<p>DARK</p>
+<div class="bg-brand-fore/0 px-2 py-4">
+	<div class="container mx-auto flex items-center justify-between">
+		<p>$DARK</p>
 		<div class="flex items-center gap-4">
 			<p>Marketplace</p>
 			<p>Powerup Shop</p>
@@ -15,12 +16,15 @@
 		</div>
 	</div>
 </div>
+<div class="relative h-0 w-full">
+	<ShineBorder />
+</div>
 
 <div class="container mx-auto">
 	<div class="py-2">
 		{#snippet eachSnippet(item: any)}
 			<div
-				class="mx-1 flex aspect-[1/1.25] w-[125px] flex-col justify-end gap-2 rounded-lg bg-cover bg-no-repeat p-1"
+				class="mx-1 flex aspect-[1/1.25] w-[125px] flex-col justify-end gap-2 rounded-lg bg-cover bg-no-repeat p-1 transition-all hover:saturate-150"
 				style="background: url('https://picsum.photos/seed/{item.name}/200/250');"
 			>
 				<p class="font-bold">{item.name}</p>
@@ -32,4 +36,19 @@
 		{/snippet}
 		<Marquee items={agents.splice(0, 5)} {eachSnippet} />
 	</div>
+</div>
+
+<div class="container mx-auto">
+	<div class="grid grid-cols-[3fr_1fr_1fr_1fr] gap-1">
+		<span class="bg-red-500"> Agent </span>
+		<span class="bg-red-500"> Performance </span>
+		<span class="bg-red-500"> Available Shares </span>
+		<span class="bg-red-500"> Prices </span>
+	</div>
+
+	{#each agents as agent}
+		<div class="grid grid-cols-[3fr_1fr_1fr_1fr] gap-1">
+			<span class=""> {agent.name} </span>
+		</div>
+	{/each}
 </div>
