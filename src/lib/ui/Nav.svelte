7e7411ac4ee5b96ec4bbff76cf5c17e5435613e7
JollyGrin <lol@dean.lol>
2025-04-10 16:53:00 +0200

    delete debugger span

--- src/lib/ui/Nav.svelte
+++ src/lib/ui/Nav.svelte
@@ -10,7 +10,6 @@
 <div class="bg-brand-fore/0 px-2 py-4">
 	<div class="text-brand-highlight container mx-auto flex items-center justify-between font-bold">
 		<p>$DARK</p>
-		<p>{page.url.pathname}</p>
 		<div class="flex items-center gap-4">
 			<a href="{base}/" class:text-brand-fore={isMarketplace}>MarketPlace</a>
 			<a href="{base}/shop" class:text-brand-fore={isShop}>Shop</a>
