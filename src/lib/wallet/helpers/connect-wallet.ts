e5be30f63b7975fb486e0150b5eb5af7672e3d2e
JollyGrin <lol@dean.lol>
2025-04-13 14:16:47 +0200

    update connect wallet with get localstorage function

--- src/lib/wallet/helpers/connect-wallet.ts
+++ src/lib/wallet/helpers/connect-wallet.ts
@@ -1,13 +1,16 @@
-import { Connection } from "@solana/web3.js";
-import { walletStore } from "$lib/wallet/walletStore.svelte";
-import toast from "svelte-french-toast";
+import { Connection } from '@solana/web3.js';
+import { walletStore } from '$lib/wallet/walletStore.svelte';
+import toast from 'svelte-french-toast';
 
-const LOCAL_STORAGE_KEY = "rsvp-walletaddress";
+const LOCAL_STORAGE_KEY = 'solana-walletaddress';
+export function getLocalStorageWalletAddress() {
+	return localStorage.getItem(LOCAL_STORAGE_KEY);
+}
 
 // Check if Phantom wallet is available
 export const checkWallet = async () => {
 	if (window.solana || window.solflare || window.backpack) {
-		console.info("Wallet found!");
+		console.info('Wallet found!');
 		if (window.solana) {
 			return window.solana;
 		} else if (window.solflare) {
@@ -16,9 +19,9 @@ export const checkWallet = async () => {
 			return window.backpack;
 		}
 	} else {
-		console.warn("Wallet not found. Please install it.");
-		toast.error("Wallet not found");
-		
+		console.warn('Wallet not found. Please install it.');
+		toast.error('Wallet not found');
+
 		return null;
 	}
 };
@@ -29,31 +32,31 @@ export const connectWallet = async (connection?: Connection) => {
 	if (!wallet) return;
 	try {
 		const response = await wallet.connect();
-		
+
 		const walletAddress = wallet.publicKey?.toString() ?? null;
 
-		if(!walletAddress) {
-			toast.error("Wallet connection failed");
-			throw new Error("Wallet connection failed");
+		if (!walletAddress) {
+			toast.error('Wallet connection failed');
+			throw new Error('Wallet connection failed');
 		}
-		
+
 		// TODO: replace this with api - rpc will be handled by backend
 		const balance = 1;
-		
+
 		// update wallet store with wallet address and balance
 		walletStore.update((store) => ({
 			...store,
 			walletAddress,
-			balance: 1,
+			balance: 1
 		}));
-		
+
 		// store wallet address in local storage
 		localStorage.setItem(LOCAL_STORAGE_KEY, walletAddress);
-		
+
 		return { walletAddress, balance };
 	} catch (error) {
-		toast.error("Wallet connection failed: " + error);
-		console.error("Wallet connection failed:", error);
+		toast.error('Wallet connection failed: ' + error);
+		console.error('Wallet connection failed:', error);
 	}
 };
 
@@ -63,19 +66,19 @@ export const disconnectWallet = async () => {
 	try {
 		// Disconnect the wallet
 		await wallet.disconnect();
-		
+
 		localStorage.removeItem(LOCAL_STORAGE_KEY);
-		
+
 		// Clear wallet information from the store
 		walletStore.update((store) => ({
 			...store,
 			walletAddress: null,
-			balance: 0,
+			balance: 0
 		}));
-		
+
 		// Optional: Clear other session data if needed
 		// For example, if you store token data or other wallet-related data in localStorage or sessionStorage
 	} catch (error) {
-		console.error("Wallet disconnection failed:", error);
+		console.error('Wallet disconnection failed:', error);
 	}
 };
